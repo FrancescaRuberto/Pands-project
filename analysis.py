379d4a111ea3afcc18db69861c0f0974a2cac044
Francesca Ruberto <francescaruberto01@gmail.com>
2024-04-30 18:33:12 +0100

    initiate summarize variables

--- analysis.py
+++ analysis.py
@@ -2,3 +2,15 @@
 # The analysis will be through a number of steps aimed at providing valuable insights on the dataset.
 # Author: Francesca Ruberto
 
+# As first step we are going to summarize variable
+# The summary will be outputted as text file to facilitate reading
+
+# Importing pandas in order to read our dataset file
+# by adding pandas we can easily maninupulate and analyse the data
+
+import pandas as pd 
+
+# I now define my new function
+def summarize_variables (summary_output):
+    # Let's upload our dataset
+    df = pd.read_csv("dataset_iris.csv")
