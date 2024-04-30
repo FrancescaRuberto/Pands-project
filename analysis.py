4f74c82467c42a37417b285f08f4b033064b94d2
Francesca Ruberto <francescaruberto01@gmail.com>
2024-04-30 18:18:35 +0100

    first input analysis

---
+++ analysis.py
@@ -0,0 +1,4 @@
+# The goal of this program is to provide a basic analysis of the famous Iris flower dataset
+# The analysis will be through a number of steps aimed at providing valuable insights on the dataset.
+# Author: Francesca Ruberto
+
